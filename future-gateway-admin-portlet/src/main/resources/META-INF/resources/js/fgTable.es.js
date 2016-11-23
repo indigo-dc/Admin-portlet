b76f17a2c1c84c5adc0f6b9f059dbc602b0f36b4
Marco Fargetta <marco.fargetta@ct.infn.it>
2016-11-23 12:13:03 +0100

    Javascript checkstyle

--- future-gateway-admin-portlet/src/main/resources/META-INF/resources/js/fgTable.es.js
+++ future-gateway-admin-portlet/src/main/resources/META-INF/resources/js/fgTable.es.js
@@ -1,6 +1,7 @@
 'use strict';
+
 import Dom from 'metal-dom/src/dom';
-import MultiMap from 'metal-structs/src/MultiMap'
+import MultiMap from 'metal-structs/src/MultiMap';
 import Datatable from 'metal-datatable/src/Datatable';
 import Modal from 'metal-modal/src/Modal';
 import Ajax from 'metal-ajax/src/Ajax';
@@ -8,117 +9,144 @@ import TreeView from 'metal-treeview/src/Treeview';
 
 
 class FgTable {
-	constructor(apiUrl = 'https://localhost/apis/v1.0 ',
-	        tableIdentifier='#application', token='') {
-		this.apiUrl = apiUrl;
-		this.tableIdentifier = tableIdentifier;
-		this.token = token;
-	}
-	
-	render (resource, columns, details_callback) {
-	    if(this.token.substring(0,4) == 'User' || this.token.substring(0,7) == 'No JSON') {
-	        Dom.append(this.tableIdentifier,'<h1 class="">No Token available.</h1>');
-	        return;
-	    }
-	    
-        var headers = new MultiMap();
-        headers.add('Authorization', 'Bearer ' + this.token);
-        headers.add('content-type', 'application/json');
-            
-        var promise = Ajax.request(this.apiUrl+'/'+resource, 'GET',null, headers, null);
-        var tableBlock = this.tableIdentifier;        
-        promise.then(function(data){
-            var tableData = JSON.parse(data.response)[resource];
-            tableData.forEach(function(entry){
-                Object.keys(entry).forEach(function(keyEntry){
-                    if(columns.indexOf(keyEntry) < 0){
-                        delete(entry[keyEntry]);
-                    }
-                    
-                });
-                columns.forEach(function(keyEntry){
-                    if (keyEntry == 'id') {
-                        entry[keyEntry] = '<a href="#' + entry[keyEntry] + '" onClick="'+ details_callback +'('+entry[keyEntry]+')">' + entry[keyEntry] + '</a>'; 
-                    }
-                    entry[keyEntry.capitalize()] = entry[keyEntry];
-                    delete(entry[keyEntry]);
-                });
-            });
-            var dt = new Datatable({'data': tableData, displayColumnsType: false, formatColumns: unsortColumns}, tableBlock);
-        });
-	}
+  constructor(apiUrl = 'https://localhost/apis/v1.0 ',
+      tableIdentifier = '#application', token = '') {
+    this.apiUrl = apiUrl;
+    this.tableIdentifier = tableIdentifier;
+    this.token = token;
+  }
 
-	showDetails(resource, id) {
-        if(this.token.substring(0,4) == 'User' || this.token.substring(0,7) == 'No JSON') {
-            var modalError = new Modal({
-                elementClasses: 'modal-boot',
-                header: '<h4 class="modal-title">Error</h4>',
-                body: 'No token available!',
-                footer: '<button type="button" class="btn btn-primary">OK</button>'
-            });
-            modalError.show();
-            return;
-        }
-        var headers = new MultiMap();
-        headers.add('Authorization', 'Bearer ' + this.token);
-        headers.add('content-type', 'application/json');
-            
-        var promise = Ajax.request(this.apiUrl + '/' + resource + '/' + id, 'GET', null, headers, null);
-        promise.then(function(data){
-            var date = new Date();
-            var resourceId = resource + id + date.getTime();
-            var modalTask = new Modal({
-                elementClasses: 'modal-boot',
-                header: '<h4 class="modal-title">' + resource.substring(0, resource.length - 1).capitalize() + ': ' + id + '</h4>',
-                body: '<div id="' + resourceId + '"></div>'
-            });
-
-            new TreeView({
-                nodes: FgTable.convertToNodes(JSON.parse(data.response))
-            }, '#' + resourceId);
-            
-            modalTask.show();
-            
-        });
-	}
-	
-	static convertToNodes(json) {
-	    var nodes = new Array();
-	    var childrenList;
-	    Object.keys(json).forEach(function(key){
-            if(typeof json[key] !== null && typeof json[key] === 'object'){
-                if(Array.isArray(json[key])){
-                    childrenList = new Array();
-                    var arrayElem = 0;
-                    json[key].forEach(function(childElem){
-                        if(typeof childElem !== null && typeof childElem === 'object'){
-                            childrenList.push({name: arrayElem++, children: FgTable.convertToNodes(childElem)});
-                        } else {
-                            childrenList.push({name: childElem});
-                        }
-                    });
-                    if(childrenList.length>0){
-                        nodes.push({name: key, children: childrenList});
-                    } else {
-                        nodes.push({name: key, children: [{name: 'N/A'}]});
-                    }
-                } else {
-                    nodes.push({name: key, children: FgTable.convertToNodes(json[key])});
-                }
-            } else {
-                nodes.push({name: key, children: [{name: json[key]}]});
-            }
-	    });
-	    return nodes;
+  render(resource, columns, detailsCallback) {
+    if (this.token.substring(0, 4) == 'User' ||
+        this.token.substring(0, 7) == 'No JSON') {
+      Dom.append(
+          this.tableIdentifier,
+          '<h1 class="">No Token available.</h1>'
+      );
+      return;
     }
-}
+
+    var headers = new MultiMap();
+    headers.add('Authorization', 'Bearer ' + this.token);
+    headers.add('content-type', 'application/json');
+
+    var resourcesCall = Ajax.request(
+        this.apiUrl + '/' + resource,
+        'GET', null, headers, null
+    );
+    var tableBlock = this.tableIdentifier;
+    resourcesCall.then(function(data) {
+      var tableData = JSON.parse(data.response)[resource];
+      tableData.forEach(function(entry) {
+        Object.keys(entry).forEach(function(keyEntry) {
+          if (columns.indexOf(keyEntry) < 0) {
+            delete(entry[keyEntry]);
+          }
+        });
+        columns.forEach(function(keyEntry) {
+          if (keyEntry == 'id') {
+            entry[keyEntry] = '<a href="#' + entry[keyEntry] +
+              '" onClick="' + detailsCallback + '(' +
+              entry[keyEntry] + ')">' + entry[keyEntry] +
+              '</a>';
+          }
+          entry[keyEntry.capitalize()] = entry[keyEntry];
+          delete(entry[keyEntry]);
+        });
+      });
+      var dt = new Datatable(
+          {
+            data: tableData,
+            displayColumnsType: false,
+            formatColumns: unsortColumns,
+          },
+        tableBlock);
+    });
+  }
+
+  showDetails(resource, id) {
+    if (this.token.substring(0, 4) == 'User' ||
+        this.token.substring(0, 7) == 'No JSON') {
+      var modalError = new Modal({
+        elementClasses: 'modal-boot',
+        header: '<h4 class="modal-title">Error</h4>',
+        body: 'No token available!',
+        footer: '<button type="button"' +
+          ' class="btn btn-primary">OK</button>',
+      });
+      modalError.show();
+      return;
+    }
+    var headers = new MultiMap();
+    headers.add('Authorization', 'Bearer ' + this.token);
+    headers.add('content-type', 'application/json');
+
+    var resourceDetailsCall = Ajax.request(this.apiUrl + '/' + resource +
+        '/' + id, 'GET', null, headers, null);
+    resourceDetailsCall.then(function(data) {
+      var date = new Date();
+      var resourceId = resource + id + date.getTime();
+      var modalTask = new Modal({
+        elementClasses: 'modal-boot',
+        header: '<h4 class="modal-title">' +
+          resource.substring(0, resource.length - 1).capitalize() +
+          ': ' + id + '</h4>',
+        body: '<div id="' + resourceId + '"></div>',
+      });
+
+      new TreeView({
+        nodes: FgTable.convertToNodes(JSON.parse(data.response)),
+      }, '#' + resourceId);
+      modalTask.show();
+    });
+  }
+
+  static convertToNodes(json) {
+    var nodes = new Array();
+    var childrenList;
+    Object.keys(json).forEach(function(key) {
+      if (typeof json[key] !== null && typeof json[key] === 'object') {
+        if (Array.isArray(json[key])) {
+          childrenList = new Array();
+          var arrayElem = 0;
+          json[key].forEach(function(childElem) {
+            if (typeof childElem !== null &&
+                typeof childElem === 'object') {
+              childrenList.push({
+                name: arrayElem++,
+                children: FgTable.convertToNodes(childElem),
+              });
+            } else {
+              childrenList.push({name: childElem});
+            }
+          });
+          if (childrenList.length > 0) {
+            nodes.push({name: key, children: childrenList});
+          } else {
+            nodes.push({name: key, children: [{name: 'N/A'}]});
+          }
+        } else {
+          nodes.push(
+              {
+                name: key,
+                children: FgTable.convertToNodes(json[key]),
+              }
+          );
+        }
+      } else {
+        nodes.push({name: key, children: [{name: json[key]}]});
+      }
+    });
+    return nodes;
+  }
+};
 
 function unsortColumns(columns) {
-    return columns
+  return columns;
 }
 
 String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
 
 export default FgTable;
